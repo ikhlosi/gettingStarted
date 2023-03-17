0e0402456dfb281cf706748124743f926c1c8e98
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-03-17 08:06:16 +0100

    Testing components & styles
    FlatList basics, Text & styles

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Cafe from "./src/components/Cafe";
+import FlatListBasics from "./src/components/FlatListBasics";
+import LotsOfStyles from "./src/components/LotsOfStyles";
+
+// const YourApp = () => {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Try editing me! 🎉</Text>
+//     </View>
+//   );
+// };
+
+// export default YourApp;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <Cafe /> */}
+      {/* <FlatListBasics /> */}
+      <LotsOfStyles />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
